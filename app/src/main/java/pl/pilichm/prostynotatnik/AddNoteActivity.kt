68ca48f551da50980d1d32310798f9474ddcc5b8
pilichm <michal.pilichowski@onet.pl>
2021-10-18 17:06:37 +0200

    Added: - layout for main and add note activity, - string, dimen and color values for created layouts.

---
+++ app/src/main/java/pl/pilichm/prostynotatnik/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package pl.pilichm.prostynotatnik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
--- app/src/main/java/pl/pilichm/prostynotatnik/MainActivity.kt
+++ app/src/main/java/pl/pilichm/prostynotatnik/MainActivity.kt
@@ -1,11 +1,41 @@
 package pl.pilichm.prostynotatnik
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Loads notes stored in shared preferences and displays it on main screen.
+     * If no note is found, default one will be displayed.
+     * */
+    private fun displaySavedNotes(){
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.main_menu_add_note -> {
+                val intent = Intent(applicationContext, AddNoteActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
